b721c9dce2fc513bbac82316f2257dd22515579f
SagynalievTaalaibek <sagynalievv.t@gmail.com>
2024-01-15 16:51:52 +0600

    add file message.ts

--- index.ts
+++ index.ts
@@ -1,2 +1,12 @@
-console.log('Hello world!');
+import express from 'express';
+
+const app = express();
+const port = 8000;
+
+app.use(express.json());
+
+
+app.listen(port, () => {
+    console.log(`Server listen on ${port} port!`);
+});
 
